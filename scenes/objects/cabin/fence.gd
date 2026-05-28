1af02bd40a882d5afd66c66fe32e750989f36974
Nahuel Giamminelli <159076064+NahuelGia@users.noreply.github.com>
2026-05-28 11:43:47 -0300

    Cabin & CabinFence
    * wip
    
    * Cabin & CabinFence
    
    ---------
    
    Co-authored-by: ngiammiKovix <ngiamminelli@kovix-group.com>

---
+++ scenes/objects/cabin/fence.gd
@@ -0,0 +1,19 @@
+class_name CabinFence
+extends StaticBody2D
+
+@export var max_health: int = 200
+
+var current_health: int
+
+signal destroyed
+
+
+func _ready() -> void:
+	current_health = max_health
+
+
+func take_damage(amount: int) -> void:
+	current_health -= amount
+	if current_health <= 0:
+		destroyed.emit()
+		queue_free()
