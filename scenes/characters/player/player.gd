463c3592317e0f40b6b9b1f2c828ff815a5b6efa
augustopiana <augustopiana3@gmail.com>
2026-06-15 23:57:31 -0300

    Visuals: Visuales de leñador, ataque, movimientos

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,9 +5,14 @@ const WOOD_PER_TREE: int = 5
 const PUSH_FORCE: float = 300.0
 const PUSH_DURATION: float = 0.3
 
+# Orden coincide con la fórmula: rotation=0 → East, cada paso +45° en sentido horario
+enum Direction { E, SE, S, SW, W, NW, N, NE }
+
 @export var move_speed: float = 250.0
 
 var wood: int = 0
+var _facing: Direction = Direction.E
+var _move_dir: Direction = Direction.E
 
 signal wood_changed(amount: int)
 signal health_changed(current: int, max_val: int)
@@ -15,6 +20,7 @@ signal died
 
 @onready var axe: Axe = $Axe
 @onready var health: HealthComponent = $HealthComponent
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _ready() -> void:
@@ -24,6 +30,7 @@ func _ready() -> void:
 	axe.hit_zombie.connect(_on_axe_hit_zombie)
 	axe.hit_zombie_push.connect(_on_axe_hit_zombie_push)
 	axe.hit_tree.connect(_on_axe_hit_tree)
+	anim_sprite.animation_finished.connect(_on_animation_finished)
 
 
 func _physics_process(_delta: float) -> void:
@@ -31,21 +38,57 @@ func _physics_process(_delta: float) -> void:
 	_face_mouse()
 	_handle_attack()
 	_handle_push()
+	_update_idle_animation()
 	move_and_slide()
 
 
 func _handle_movement() -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * move_speed
+	if direction.length_squared() > 0.0:
+		_move_dir = _angle_to_direction(direction.angle())
 
 
 func _face_mouse() -> void:
 	look_at(get_global_mouse_position())
+	_facing = _angle_to_direction(rotation)
+	anim_sprite.rotation = -rotation
+
+
+func _angle_to_direction(angle: float) -> Direction:
+	var deg := fposmod(rad_to_deg(angle) + 22.5, 360.0)
+	match int(deg / 45.0) % 8:
+		0: return Direction.E
+		1: return Direction.SE
+		2: return Direction.S
+		3: return Direction.SW
+		4: return Direction.W
+		5: return Direction.NW
+		6: return Direction.N
+		7: return Direction.NE
+	return Direction.E
 
 
 func _handle_attack() -> void:
 	if Input.is_action_just_pressed("attack"):
 		axe.swing()
+		var anim := _get_attack_anim()
+		if anim != "":
+			anim_sprite.speed_scale = 2.5
+			anim_sprite.play(anim)
+
+
+func _get_attack_anim() -> String:
+	match _facing:
+		Direction.N:  return "attack_north"
+		Direction.NE: return "attack_north_east"
+		Direction.E:  return "attack_east"
+		Direction.SE: return "attack_south_east"
+		Direction.S:  return "attack_south"
+		Direction.SW: return "attack_south_west"
+		Direction.W:  return "attack_west"
+		Direction.NW: return "attack_north_west"
+		_: return ""
 
 
 func _handle_push() -> void:
@@ -78,8 +121,49 @@ func upgrade_armor(health_bonus: int) -> void:
 	health_changed.emit(health.current_health, health.max_health)
 
 
+func _update_idle_animation() -> void:
+	var is_moving := velocity.length_squared() > 0.0
+	if anim_sprite.animation.begins_with("attack") and anim_sprite.is_playing() and not is_moving:
+		return
+	anim_sprite.speed_scale = 1.0
+	var anim := _get_walk_anim() if is_moving else _get_idle_anim()
+	if anim != "":
+		anim_sprite.play(anim)
+	else:
+		anim_sprite.stop()
+
+
+func _get_walk_anim() -> String:
+	match _move_dir:
+		Direction.N:  return "walk_north"
+		Direction.NE: return "walk_north_east"
+		Direction.E:  return "walk_east"
+		Direction.SE: return "walk_south_east"
+		Direction.S:  return "walk_south"
+		Direction.SW: return "walk_south_west"
+		Direction.W:  return "walk_west"
+		Direction.NW: return "walk_north_west"
+		_: return _get_idle_anim()
+
+
+func _get_idle_anim() -> String:
+	match _facing:
+		Direction.N:  return "idle_north"
+		Direction.NE: return "idle_north_east"
+		Direction.E:  return "idle_east"
+		Direction.SE: return "idle_south_east"
+		Direction.S:  return "idle_south"
+		Direction.SW: return "idle_south_west"
+		Direction.W:  return "idle_west"
+		Direction.NW: return "idle_north_west"
+		_: return ""
+
+
+func _on_animation_finished() -> void:
+	_update_idle_animation()
+
+
 func _on_axe_hit_zombie(zombie: Zombie) -> void:
-	print("Hit - Zombie")
 	zombie.take_damage(axe.damage)
 
 
