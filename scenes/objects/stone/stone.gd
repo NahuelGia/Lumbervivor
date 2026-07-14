f68ce8041525f84f3450a4a0df8aabe522fbd2a5
augustopiana <augustopiana3@gmail.com>
2026-07-13 22:45:11 -0300

    stone hit sound and menu bug

--- scenes/objects/stone/stone.gd
+++ scenes/objects/stone/stone.gd
@@ -1,14 +1,27 @@
 class_name ChoppableStone
 extends Destructible
 
+const HIT_SOUNDS: Array = [
+	preload("res://assets/audio/stone_hit.wav"),
+	preload("res://assets/audio/stone_hit2.wav"),
+	preload("res://assets/audio/stone_hit3.wav"),
+]
+
 signal mined
 
 @onready var health_bar: HealthBar = $HealthBar
+@onready var hit_player: AudioStreamPlayer2D = $HitPlayer
 
 
 func _ready() -> void:
 	super._ready()
 	health.health_changed.connect(health_bar.update_health)
+
+
+func take_damage(amount: int) -> void:
+	super.take_damage(amount)
+	hit_player.stream = HIT_SOUNDS[randi() % HIT_SOUNDS.size()]
+	hit_player.play()
 
 
 func _on_died() -> void:
--- scenes/ui/pause/pause_menu.gd
+++ scenes/ui/pause/pause_menu.gd
@@ -56,4 +56,5 @@ func _on_restart_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	hud_ok_sound.play()
-	get_tree().quit()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu/main_menu.tscn")
